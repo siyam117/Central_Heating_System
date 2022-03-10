665ab9f9ac6cbaf67d750b74fd5b9e5db2858dfc
Siyam Muhammad <siyam.muhammad7@gmail.com>
2022-03-10 19:48:21 +0000

    Menu Edited with smoother drag and drop functionality

--- menu.js
+++ menu.js
@@ -6,178 +6,226 @@ const droppableArea = document.querySelector('#drop-area')
 const dropAreaText = document.querySelector('#drop-area-text')
 let roomNumber = 0
 
-// console.log(droppableArea.id)
-// //console.log(dropAreaText.nodeName)
-// $(".main-room").draggable({
-//     containment: "document",
-//     snap: true,
-//     snapTolerance: 50,
-//     stop: mainRoomDragStop
-// });
-
-// $(".bathroom").draggable({
-//     containment: "document",
-//     snap: true,
-//     snapTolerance: 50,
-//     stop: bathroomDragStop
-// });
-
-// $('#drop-area').droppable( {
-//     snap: true,
-//     snapTolerance: 20,
-//     drop: handleDropEvent
-//   });
-
-// function handleDropEvent( event, ui ) {
-// var draggable = ui.draggable;
-
-// }
 
 
-// function mainRoomDragStop( event, ui ) {
-//     var offsetXPo = parseInt( ui.offset.left );
-//     var offsetYPos = parseInt( ui.offset.top );
-//     const droppedElement = document.getElementById(draggableMainRooom.id)
-
-//     droppableArea.appendChild(droppedElement)
-//     // droppableArea.appendChild(droppedElement).style.top = '0'
-//      droppableArea.appendChild(droppedElement).style.margin = '0'
-//   }
-
-//   function bathroomDragStop( event, ui ) {
-//     var offsetXPos = parseInt( ui.offset.left );
-//     var offsetYPos = parseInt( ui.offset.top );
-//     const droppedElement = document.getElementById(draggableBathroom.id)
-
-//     droppableArea.appendChild(droppedElement)
-//     // droppableArea.appendChild(droppedElement).style.top = '0'
-//      droppableArea.appendChild(droppedElement).style.margin = '0'
+$(function(){
+    count = 0
+    var canvas = $("#drop-area")
+    var $dragRoom = $("#room")
+    var diagram = [];
+    const draggableMainRooom = document.querySelector('#main-room')
 
 
-//   }
-
-draggableMainRooom.addEventListener('dragstart', e=>{
-    e.dataTransfer.setData('text/plain',draggableMainRooom.id)
-    console.log(e.dataTransfer.setData('text/plain',draggableMainRooom.id)
-    )
-    roomNumber = 1
+$('.sub-btn').click(function(){
+    $(this).next('.sub-menu').slideToggle();
+    $(this).find('.dropdown').toggleClass('rotate');
 })
 
-draggableBathroom.addEventListener('dragstart', e=>{
-    console.log(e.dataTransfer.setData('text/plain',draggableBathroom.id))
-    roomNumber = 2
-})
+$("div","#room").draggable({
+    containment: "document",
+    revert: "invalid",
+    helper: "clone",
+    cursor: "move",
+    
 
-draggableLivingRoom.addEventListener('dragstart', e=>{
-    console.log(e.dataTransfer.setData('text/plain',draggableLivingRoom.id))
-    roomNumber = 3
-})
-const ROOM = document.querySelector('#room')
-const DRAGGABLE_AREA = document.querySelector('drop-area')
-var dragItem = null;
+});
 
 
 
+canvas.droppable({
+    drop: function(event,ui){
+        var node = {
+            _id: (new Date).getTime(),
+            position: ui.helper.position(),
 
-//when draggable object is over the drop area
-for (const dropArea of document.querySelectorAll(".drop-area")){
-    dropArea.addEventListener('dragover', e=>{
-        e.preventDefault();
-        dropArea.classList.add("drop-zone-over");
-    })
-    //when draggable element is dropped onto the drop area
-    dropArea.addEventListener('drop', e=>{
-        e.preventDefault();
-        
-        const droppedElementId = e.dataTransfer.getData("text/plain")
-        const droppedElement = document.getElementById(droppedElementId)
-        
-        if(roomNumber ===  1){
-            droppableArea.appendChild(droppedElement).style.width = '200px'
-            droppableArea.appendChild(droppedElement).style.height = '250px'
-            droppableArea.appendChild(droppedElement).style.display = 'flexbox'
-            droppableArea.appendChild(droppedElement).style.left = '0'
-            droppableArea.appendChild(droppedElement).style.top = '0'
-            droppableArea.appendChild(droppedElement).style.marginTop = '0'
-            droppableArea.appendChild(droppedElement).style.resize = 'verticle'
-            droppableArea.appendChild(droppedElement).style.overflow = 'auto'
+          
 
-            dropArea.classList.remove("drop-zone-over");
+        };
+        console.log("NODE HERE")
+        console.log(node)
 
+        node.position.left -= canvas.position().left;
 
-            //dropAreaText.innerHTML = ''
-        }else if(roomNumber === 2){
-            droppableArea.appendChild(droppedElement).style.width = '100px'
-            droppableArea.appendChild(droppedElement).style.height = '200px'
-            droppableArea.appendChild(droppedElement).style.display = 'flexbox'
-            droppableArea.appendChild(droppedElement).style.left = '0'
-            droppableArea.appendChild(droppedElement).style.top = '0'
-            droppableArea.appendChild(droppedElement).style.marginTop = '0'
-            droppableArea.appendChild(droppedElement).style.resize = 'verticle'
-            droppableArea.appendChild(droppedElement).style.overflow = 'auto'
+        if(ui.helper.hasClass("main-room")){
+            node.type = "MAIN-ROOM"
+         
+        }else if(ui.helper.hasClass("bathroom")){
+            node.type = "BATHROOM"
 
-            dropArea.classList.remove("drop-zone-over");
-            //dropAreaText.innerHTML = ''
-        }else if(roomNumber === 3){
-            droppableArea.appendChild(droppedElement).style.width = '400px'
-            droppableArea.appendChild(droppedElement).style.height = '200px'
-            droppableArea.appendChild(droppedElement).style.display = 'flexbox'
-            droppableArea.appendChild(droppedElement).style.left = '0'
-            droppableArea.appendChild(droppedElement).style.top = '0'
-            droppableArea.appendChild(droppedElement).style.marginTop = '0'
-            droppableArea.appendChild(droppedElement).style.resize = 'verticle'
-            droppableArea.appendChild(droppedElement).style.overflow = 'auto'
-            dropArea.classList.remove("drop-zone-over");
-            //dropAreaText.innerHTML = ''
+        }else if(ui.helper.hasClass("living-room")){
+            node.type = "LIVING-ROOM"
+            
         }
-        
-        
-        
-    })
-
-}
-//when draggable object is over the MENU
-for(const dropArea of document.querySelectorAll(".menu")){
-    dropArea.addEventListener('dragover', e=>{
-        e.preventDefault();
-        
-    })
-    //when draggable element is dropped onto the MENU
-    dropArea.addEventListener('drop', e=>{
-        e.preventDefault();
-
-        const menuDroppedElementId = e.dataTransfer.getData("text/plain")
-        const menuDroppedElement = document.getElementById(menuDroppedElementId)
-
-        if(roomNumber === 1){
-        document.getElementById('room').appendChild(menuDroppedElement).style.width = '100px'
-        document.getElementById('room').appendChild(menuDroppedElement).style.height = '100px'
-        document.getElementById('room').appendChild(menuDroppedElement).style.left = ''
-        document.getElementById('room').appendChild(menuDroppedElement).style.marginTop = '10'
-        document.getElementById('main-room').style.display = 'block'
-        droppableArea.classList.remove('drop-zone-over')
-
-        } else if (roomNumber === 2){
-            document.getElementById('room').appendChild(menuDroppedElement).style.width = '50px'
-            document.getElementById('room').appendChild(menuDroppedElement).style.height = '100px'
-            document.getElementById('room').appendChild(menuDroppedElement).style.left = ''
-            document.getElementById('room').appendChild(menuDroppedElement).style.marginTop = '10'
-            document.getElementById('bathroom').style.display = 'block'
-            droppableArea.classList.remove('drop-zone-over')
+        else{
+            return;
         }
+        diagram.push(node)
+        console.log(diagram)
+        renderDiagram(diagram)
+       
+    }
+})
 
-        
+function renderDiagram(diagram){
+    canvas.empty();
+    for(var d in diagram){
+        var node = diagram[d];
+        var html = "";
+        console.log("DIAGRAM CONSOLED HERE")
+        console.log(diagram)
+        if(node.type === "MAIN-ROOM"){
+            html = "<div>Bedroom</div>";
+            var dom = $(html).css({
+                "font-size": "15px",
+                "text-align": "center",
+                "line-height": "100px",
+                "cursor": "pointer",
+                "width": "250px",
+                "height": "250px",
+                "border": "8px solid black",
+                "background-color": "white",
+                "position": "absolute",
+                "top": node.position.top,
+                "left": node.position.left
+            }).draggable({
+                snap: true,
+                snapTolerance: 30,
+                stop: function(event,ui){
+                    console.log(ui)
+                    var id = ui.helper.attr("id")
+                    for(var i in diagram){
+                        if(diagram[i]._id == id){
+                            diagram[i].position.top = ui.position.top
+                            diagram[i].position.left = ui.position.left
+                        }
+                    }
+                },
+                containment: "#drop-area",
+                snap: true,
+                snapTolerance: 10
+            }).attr("id",node._id).resizable({                               //fix the resize using the same methods as draggable
+                containment: "#drop-area",                                   //i.e. make it so that it doesnt move when another room is dropped after resized
+                minWidth: 100,
+                minHeight: 100,
+                start: function(event,ui){
+                    console.log("LOGGING UI HERE")
+                    console.log(ui)
+                    
+                    var id = ui.helper.attr("id")
+                    for(var i in diagram){
+                        if(diagram[i]._id == id){
+                            console.log(diagram[i].width,ui.size.width)
+                            console.log(diagram[i].height,ui.size.height)
+                            diagram[i].width = ui.size.width
+                            diagram[i].height = ui.size.height
+                        }
+                    }
+                }
+            })
 
+            canvas.append(dom)
+        }else if(node.type === "BATHROOM"){
+            html = "<div>Bath</div>";
+            var dom = $(html).css({
+                "font-size": "15px",
+                "text-align": "center",
+                "line-height": "100px",
+                "cursor": "pointer",
+                "width": "100px",
+                "height": "200px",
+                "border": "8px solid black",
+                "background-color": "white",
+                "position": "absolute",
+                "top": node.position.top,
+                "left": node.position.left
+            }).draggable({
+                snap: true,
+                snapTolerance: 30,
+                stop: function(event,ui){
+                    console.log(ui)
+                    var id = ui.helper.attr("id")
+                    for(var i in diagram){
+                        if(diagram[i]._id == id){
+                            diagram[i].position.top = ui.position.top
+                            diagram[i].position.left = ui.position.left
+                        }
+                    }
+                },
+                containment: "#drop-area",
+                snap: true,
+                snapTolerance: 10
+            }).attr("id",node._id).resizable({                               //fix the resize using the same methods as draggable
+                containment: "#drop-area",                                   //i.e. make it so that it doesnt move when another room is dropped after resized
+                start: function(event,ui){
+                    
+                    var id = ui.helper.attr("id")
+                    for(var i in diagram){
+                        if(diagram[i]._id == id){
+                            console.log(diagram[i].width,ui.size.width)
+                            console.log(diagram[i].height,ui.size.height)
+                            diagram[i].width = ui.size.width
+                            diagram[i].height = ui.size.height
+                        }
+                    }
+                }
+            })
 
-
-
-        
-    })
-
+            canvas.append(dom)
+        }else if(node.type === "LIVING-ROOM"){
+            html = "<div>living-room</div>";
+            var dom = $(html).css({
+                "font-size": "15px",
+                "text-align": "center",
+                "line-height": "100px",
+                "cursor": "pointer",
+                "width": "400px",
+                "height": "300px",
+                "border": "8px solid red",
+                "background-color": "white",
+                "position": "absolute",
+                "top": node.position.top,
+                "left": node.position.left
+            }).draggable({
+                stop: function(event,ui){
+                    console.log(ui)
+                    var id = ui.helper.attr("id")
+                    for(var i in diagram){
+                        if(diagram[i]._id == id){
+                            diagram[i].position.top = ui.position.top
+                            diagram[i].position.left = ui.position.left
+                        }
+                    }
+                },
+                containment: "#drop-area",
+                snap: true,
+                snapTolerance: 10
+            }).attr("id",node._id).resizable({                               //fix the resize using the same methods as draggable
+                containment: "#drop-area", 
+                minWidth: 100,
+                minHeight: 100,                                  //i.e. make it so that it doesnt move when another room is dropped after resized
+                start: function(event,ui){
+                    
+                    var id = ui.helper.attr("id")
+                    for(var i in diagram){
+                        if(diagram[i]._id == id){
+                            console.log(diagram[i].width,ui.size.width)
+                            console.log(diagram[i].height,ui.size.height)
+                            diagram[i].width = ui.size.width
+                            diagram[i].height = ui.size.height
+                        }
+                    }
+                }
+            })
+            canvas.append(dom)
+        }
+    }
 }
+})
 
 
 
+
+//---------------------------------------------------------------------------------------------------------------------------------------
 
 
 
